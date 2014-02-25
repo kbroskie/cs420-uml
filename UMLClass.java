fb952ed0db262b0c24df582fe4d9ba818bb6fb17
Lindsay <lindsayrblank@gmail.com>
2014-02-25 12:33:14 -0500

    Create UMLClass.java
    Subclass of UMLObject to create a UML class object.

---
+++ UMLClass.java
@@ -0,0 +1,28 @@
+public class UMLClass extends UMLObject {
+
+	//*********************************************************************
+	//Empty constructor
+	//x1, y1, and z1, are the coordinates of the line to be drawn in the 
+	//middle. The width will be the same as the superclass. Height?
+	public UMLClass () 
+	{
+		super();
+		this.x1 = 0;
+		this.y1 = 0;
+		this.z1	= 0;
+	}
+
+	//Copy constructor
+	public UMLClass (UMLClass source)
+	{
+		super(UMLObject source);
+		this.x1 = source.getX();
+		this.y1 = source.getY();
+		this.z1 = source.getZ();
+	}
+
+	//Modification constructor
+
+	//**********************************************************************
+
+	//Mutators
