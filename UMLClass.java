6adfa466619a4a0efd9c33f131ffd00c4eaadb0e
Lindsay <lindsayrblank@gmail.com>
2014-02-26 13:48:41 -0500

    Update UMLClass.java
    UMLClass as of right now.

--- UMLClass.java
+++ UMLClass.java
@@ -1,42 +1,62 @@
-public class UMLClass extends UMLObject {
 
-	//*********************************************************************
-	//Empty constructor
-	public UMLClass () 
-	{
+	public class UMLClass extends UMLObject {
+		int line1Height;
+		int line2Height;
+		int line1Y;
+		int line2Y;
+		
+		//*********************************************************************
+		//Empty constructor
+		public UMLClass ()
+		{
 		super();
-	}
-
-	//Copy constructor
-	public UMLClass (UMLClass source)
-	{
-		super(UMLObject source);
-	}
-
-	//Dimension constructor.
-	public UMLClass (UMLObject source, int changeType, int width, int height) {
-		super(UMLObject source, int changeType, int width, int height);
-		int line1Height = height/2;
-		int line2Height = height/4;
-	}
+		line1Height = 0;
+		line2Height = 0;
+		line1Y = 0;
+		line2Y = 0;
 	
-	//Position constructor.
-	public UMLClass (UMLObject source, int changeType, int x, int y, int z) {
-		super(UMLObject source, int changeType, int x, int y, int z);
-		int line1Y = y + line1Height;
-		int line2Y = y + line2Height;
-	}
-	
-	//Serialization?!?!?!
-	
-	//Modification constructors?!??!
+		}
 
-	//**********************************************************************
+		//Copy constructor
+		public UMLClass (UMLClass source)
+		{
+			super(source);
+		}
 
-	//Mutators
-	
-	public UMLClass changeLabel (String newLabel)
-	{
-		super.changeLabel(String newLabel);
+		//Dimension constructor.
+		public UMLClass (UMLObject source, int changeType, int width, int height) 
+				throws Exception 
+				{
+					super(source, changeType, width, height);
+					this.line1Height = height/2;
+					this.line2Height = height/4;
+				}
+
+		//Position constructor.
+		public UMLClass (UMLObject source, int changeType, int x, int y, int z, int line1Height, int line2Height) 
+				throws Exception 
+		{
+			//
+			super(source, changeType, x, y, z);
+			this.line1Y = y + line1Height;
+			this.line2Y = y + line2Height;
+		}
+
+		//**********************************************************************
+		
+		public int getLine1Y ()
+		{
+			return line1Y;
+		}
+		//**********************************************************************
+		// We are attempting to draw this thing.
+		
+		public void drawClass (Graphics g) 
+		{
+			super.draw(g);
+			g.setColor(Color.black);
+			g.drawLine(x, line1Y, x + width, line1Y);
+		}
+
 	}
 }
