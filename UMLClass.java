59d24b55c623e3be7a85f92ba42771f3aa52f4c4
Lindsay <lindsayrblank@gmail.com>
2014-02-26 12:11:19 -0500

    Update UMLClass.java
    Constructors are done (maybe).

--- UMLClass.java
+++ UMLClass.java
@@ -13,7 +13,21 @@ public class UMLClass extends UMLObject {
 		super(UMLObject source);
 	}
 
-	//Modification constructor
+	//Dimension constructor.
+	public UMLClass (UMLObject source, int changeType, int width, int height) {
+		super(UMLObject source, int changeType, int width, int height);
+		int line1Height = height/2;
+		int line2Height = height/4;
+	}
+	
+	//Position constructor.
+	public UMLClass (UMLObject source, int changeType, int x, int y, int z) {
+		super(UMLObject source, int changeType, int x, int y, int z);
+		int line1Y = y + line1Height;
+		int line2Y = y + line2Height;
+	}
+	
+	//Serialization?!?!?!
 
 	//**********************************************************************
 
