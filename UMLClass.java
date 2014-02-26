e156a528a4b089a453a7b1802e3f2b9dc70ed7a0
Lindsay <lindsayrblank@gmail.com>
2014-02-26 14:09:15 -0500

    Update UMLClass.java
    Mutators

--- UMLClass.java
+++ UMLClass.java
@@ -1,4 +1,3 @@
-
 	public class UMLClass extends UMLObject {
 		int line1Height;
 		int line2Height;
@@ -23,32 +22,64 @@
 			super(source);
 		}
 
-		//Dimension constructor.
-		public UMLClass (UMLObject source, int changeType, int width, int height) 
+		//Member constructor.
+		public UMLClass (String nLabel, int nX, int nY, int nZ, int nWidth, int nHeight, boolean nSelected) 
 				throws Exception 
 				{
-					super(source, changeType, width, height);
+					super(nLabel, nX, nY, nZ, nWidth, nHeight, nSelected);
 					this.line1Height = height/2;
 					this.line2Height = height/4;
+					this.line1Y = y + line1Height;
+					this.line2Y = y + line2Height;
 				}
 
-		//Position constructor.
-		public UMLClass (UMLObject source, int changeType, int x, int y, int z, int line1Height, int line2Height) 
-				throws Exception 
-		{
-			//
-			super(source, changeType, x, y, z);
-			this.line1Y = y + line1Height;
-			this.line2Y = y + line2Height;
-		}
-
 		//**********************************************************************
+		//Observers
+		
+		public int getL1Height () 
+		{
+			return this.line1Height;
+		}
+		
+		public int getL2Height ()
+		{
+			return this.line2Height;
+		}
 		
 		public int getLine1Y ()
 		{
-			return line1Y;
+			return this.line1Y;
 		}
+		
+		public int getLine2Y ()
+		{
+			return this.line2Y;
+		}
+		
 		//**********************************************************************
+		//Mutators
+		
+		//changelabel
+		
+		//Move
+		
+		public UMLClass moveClass (int x, int y, int z)
+		{
+			UMLClass newClass (source); //"this" 
+			super.move(x, y, z);
+		}
+		
+		//Resize
+		public UMLClass resizeClass (int width, int height)
+		{
+			UMLClass newClass (source);
+			super.resize(width, height);
+			line1Height = getLine1Height(); //?
+		}
+		
+	
+		
+		
 		// We are attempting to draw this thing.
 		
 		public void drawClass (Graphics g) 
