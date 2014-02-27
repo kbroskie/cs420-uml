ecfa9d546e54d394cae9e620050f1007899857fa
Daniel Rabiega <rabiega.dan@gmail.com>
2014-02-27 17:53:13 -0500

    Added JUnit test for ImmutablePoint

---
+++ src/edu/millersville/cs/segfault/tests/PointTest.java
@@ -0,0 +1,24 @@
+package edu.millersville.cs.segfault.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.millersville.cs.segfault.immutable.ImmutablePoint;
+
+public class PointTest {
+
+	@Test
+	public void test() {
+		ImmutablePoint testOrigin = new ImmutablePoint();
+		assertTrue("Origin point not at origin", testOrigin.getX()==0 &&
+				testOrigin.getY()==0);
+		
+		ImmutablePoint testPoint = new ImmutablePoint(3,4);
+		assertTrue("Test Point at wrong coordinates", testPoint.getX()==3 &&
+				testPoint.getY()==4);
+		
+		assertTrue("Distance calculation incorrect.", 
+				testPoint.distance(testOrigin)==5);
+	}
+}
