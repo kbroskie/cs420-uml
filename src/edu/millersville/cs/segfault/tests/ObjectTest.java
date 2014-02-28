ab5d0ce28b776c35b2f48488ee2ec1830db79c95
Benjamin K Nothstein <bknothstein@cook.cs.millersville.edu>
2014-02-28 16:38:46 -0500

    ObjectTest touched up

--- src/edu/millersville/cs/segfault/tests/ObjectTest.java
+++ src/edu/millersville/cs/segfault/tests/ObjectTest.java
@@ -1,3 +1,5 @@
+//Tests for the class UMLObject
+
 package edu.millersville.cs.segfault.tests;
 
 import static org.junit.Assert.*;
@@ -13,17 +15,32 @@ public class ObjectTest {
 	public void test() throws Exception {
 		UMLModel testModel = new UMLModel();
 		
+		//**********************************************************************************//
+		//Initializing Tests
+		//**********************************************************************************//
+		
 		assertTrue(testModel.getObjects().isEmpty());
 		UMLObject new1 = new UMLObject();
 		UMLObject new2 = new UMLObject();
-		//testModel.addObject(new1);
-		//testModel.addObject(new2);
+		UMLObject new3 = new UMLObject();
 		testModel.add(new1);
 		testModel.add(new2);
 		assertTrue(new1.getLabel() == new2.getLabel());
 		
+		//**********************************************************************************//
+		//Serialization Tests
+		//**********************************************************************************//
 		
+		new3 = new3.move(20, 20, 0);
+		UMLObject new4 = new UMLObject(new3.serialize());
+		assertTrue("Failed to take in and make object from serialized form", 
+				new4.getX() == new3.getX());
+		
+		
+		//**********************************************************************************//
 		//Move and Resize Tests
+		//**********************************************************************************//
+		
 		new1 = new1.move(20, 20, 0);
 		new2 = new2.move(100, 100, 0);
 		new1 = new1.resize(20, 50);
@@ -35,48 +52,24 @@ public class ObjectTest {
 		assertTrue(new1.getZ() == 0);
 		assertTrue(new2.getHeight() == 70);
 		assertTrue(new2.getWidth() == 60);
-		//assertTrue(new1.getSize() == new2.getSize());
 		
-		
+		//**********************************************************************************//
 		//Selection Tests
+		//**********************************************************************************//
+		
 		new1 = new1.select();
 		assertTrue("Object was not selected!", new1.isSelected());
 		new1 = new1.unselect();
 		assertFalse("Object was not unselected!", new1.isSelected());
 		
+		//**********************************************************************************//
 		//Label tests
+		//**********************************************************************************//
+		
 		new1 = new1.changeLabel("Label1");
 		assertTrue(new1.getLabel() == "Label1");
 		
-		
-		//assertTrue("", new1.within());
-		
-		
-		
-		
-		
-		
-		//System.out.println(testModel.add(new1));
-		
-		
-		
-		//testModel.removeObject(oldObject);
-		/*
-		int i = 10;
-		while(testModel.getObjects().iterator().hasNext())
-		{
-			testModel.getObjects().iterator().next().move(5*i, 5*i, 5*i);
-			System.out.print(testModel.getObjects().iterator().next().getOrigin());
-			
-			
-			i = i + 10;
-		}
-		*/
-		//testModel.getObjects().iterator().next().getX();
-		
-		
-		//System.out.println(testModel.getObjects());
-		//testModel = testModel.move(30, 20, 0);
+		//**********************************************************************************//
 		
 	}
 
