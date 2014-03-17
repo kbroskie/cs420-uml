1dcc27aa1d893e6bb6846c5ba070446656d43127
creativebluebutterfly <kbroskie@gmail.com>
2014-03-17 17:23:52 -0400

    Created subclass of JMenuBar

--- src/edu/millersville/cs/segfault/ui/WindowUI.java
+++ src/edu/millersville/cs/segfault/ui/WindowUI.java
@@ -2,7 +2,6 @@ package edu.millersville.cs.segfault.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Event;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,21 +9,16 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
-
 
 import edu.millersville.cs.segfault.model.DrawableType;
+
 import javax.swing.border.TitledBorder;
-import edu.millersville.cs.segfault.model.UMLModel;
+
 
 public class WindowUI extends JPanel 
 			implements ActionListener {
@@ -34,26 +28,11 @@ public class WindowUI extends JPanel
 	private WindowController winController;
 	private UMLPanel umlPanel;
 	
-	// Menu Items
-	/**** FILE */
-	private static final String fileMenuText = "File";
-	private static final String newMenuText = "New";
-	private static final String openMenuText = "Open";
-	private static final String saveMenuText = "Save";
-	private static final String saveAsMenuText = "Save as...";
-	private static final String exitMenuText = "Exit";
-	
-	/**** EDIT */
-	private static final String editMenuText = "Edit";
-	private static final String undoMenuText = "Undo";
-	private static final String redoMenuText = "Redo";
-	private static final String selectAllText = "Select/Deselect All";
-	
 	// Options Pane Items
 	private static final String optionsPaneObjectDraw = "Object";
 	private static final String optionsPaneRelationDraw = "Relation";
 	private static final String optionsPaneSelect = "Select";
-
+		
 	
 	/**
 	 * WindowUI constructor that constructs a window
@@ -64,10 +43,11 @@ public class WindowUI extends JPanel
 		winController = wController;
 		JPanel mainUIPanel = setMainPanelLayout();
 		winFrame.getContentPane().add(mainUIPanel, BorderLayout.CENTER);
-		buildMenu(winFrame);
 		buildUMLPanel(winFrame);
+		winFrame.setJMenuBar (new UMLMenuBar(winFrame, umlPanel));
 		buildOptionsPanel(winFrame);
 	}
+	
 	
 	/** 
 	 * Returns the current UML model.
@@ -90,89 +70,6 @@ public class WindowUI extends JPanel
 	    return mainPanel;
 	}
 		
-	/*
-	 * Builds the menu bar.
-	 * @param wFrame the frame for the interface
-	 */
-	private void buildMenu(JFrame wFrame) {
-	   JMenuBar menuBar = new JMenuBar();
-	   
-	   // Create the sub-menus.
-	   JMenu fileSubmenu = buildFileMenu();
-	   JMenu editSubmenu = buildEditMenu();
-	   
-	   // Add the menus to the menu bar.
-	   menuBar.add(fileSubmenu);
-	   menuBar.add(editSubmenu);
-	   
-	   // Set the window's menu bar.
-	   wFrame.setJMenuBar(menuBar);
-	}
-	
-	/**
-	 * Builds the File menu.
-	 * @return returns the file submenu
-	 */
-	private JMenu buildFileMenu() { 
-	   // Create the menu items, adding action listeners and accelerators.
-	   JMenu fileMenu = new JMenu(fileMenuText);
-	   
-	   JMenuItem newItem = new JMenuItem(newMenuText);
-	   newItem.addActionListener(this);
-	   newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, Event.CTRL_MASK));
-	   
-	   JMenuItem openItem = new JMenuItem(openMenuText);
-	   openItem.addActionListener(this);
-	   openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, Event.CTRL_MASK));
-
-	   JMenuItem saveItem = new JMenuItem(saveMenuText);
-	   saveItem.addActionListener(this);
-	   saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));
-	   
-	   JMenuItem saveAsItem = new JMenuItem(saveAsMenuText);
-	   saveAsItem.addActionListener(this);
-
-	   JMenuItem exitItem = new JMenuItem(exitMenuText);
-	   exitItem.addActionListener(new ExitListener());
-	   exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, Event.CTRL_MASK));
-
-	   // Add the sub-menus to the File menu.
-	   fileMenu.add(newItem);
-	   fileMenu.add(openItem);
-	   fileMenu.add(saveItem);
-	   fileMenu.add(saveAsItem);
-	   fileMenu.add(exitItem);
-	   
-	   return fileMenu;
-	}
-
-	/*
-	 * Builds the edit sub-menu.
-	 * @return returns the edit submenu
-	 */
-	private JMenu buildEditMenu() {
-	   // Create the edit submenu items, and add accelerators.
-	   JMenu editMenu = new JMenu(editMenuText);
-	   
-	   JMenuItem undoItem = new JMenuItem(undoMenuText);
-	   undoItem.addActionListener(this);
-	   undoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, Event.CTRL_MASK));
-
-	   JMenuItem redoItem = new JMenuItem(redoMenuText);
-	   redoItem.addActionListener(this);
-	   redoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, Event.CTRL_MASK));
-	   
-	   JMenuItem selectAllItem = new JMenuItem(selectAllText);
-	   selectAllItem.addActionListener(this);
-	   selectAllItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, Event.CTRL_MASK));
-	   
-	   // Add the sub-menus to the Edit menu.
-	   editMenu.add(undoItem);
-	   editMenu.add(redoItem);
-	   editMenu.add(selectAllItem);
-	   
-	   return editMenu;
-	}
 	
 	/**
 	 * Builds the UML panel.
@@ -183,6 +80,7 @@ public class WindowUI extends JPanel
 		umlPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 		wFrame.add(umlPanel, BorderLayout.CENTER);
 	}
+	
 	
 	/**
 	 * Creates a panel to place the various UML object selections.
@@ -233,6 +131,7 @@ public class WindowUI extends JPanel
 	   // Add the optionsPanel to the main frame.
 	   wFrame.add(optionsPanel, BorderLayout.WEST);
 	} 
+
 	
 	/**
 	 * Builds a panel to hold all the object options for the main options panel.
@@ -252,6 +151,7 @@ public class WindowUI extends JPanel
 		objectsPanel.add(objectButton, BorderLayout.CENTER);		
 	}
 	
+	
 	/**
 	 * Builds a panel to hold all the relations options for the main options panel.
 	 * @param relationsPanel Holds all the relation options a user can select.
@@ -270,46 +170,16 @@ public class WindowUI extends JPanel
 		relationsPanel.add(relationButton);
 	}	
 	
-	/**
-	 * Handles the event generated when the user selects 
-	 * the exit option from the File menu.
-	 * @param e the event information triggered by selecting the exit option
-	 */
-	private class ExitListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			System.exit(0);
-		}
-	}
 	
 	/**
 	 * Handles the events generated by the user selecting
-	 * a menu option.
+	 * a button.
 	 * @param se the selected event and source 
 	 */
 	 public void actionPerformed(ActionEvent se) {
 		 String selectedCommand = se.getActionCommand();
-		 	 	 
-		 if (selectedCommand == newMenuText) {
-			 umlPanel.changeModel(new UMLModel());
-		 }
-		 else if (selectedCommand == openMenuText) {
-			 umlPanel.load();
-		 }
-		 else if (selectedCommand == saveMenuText) {
-			 umlPanel.save(umlPanel.model().serialize());
-		 }
-		 else if (selectedCommand == saveAsMenuText) {
-			 umlPanel.saveAs(umlPanel.model().serialize());
-		 }
-		 else if (selectedCommand == undoMenuText) {
-			 umlPanel.undo();	
-		 }
-		 else if (selectedCommand == redoMenuText) {
-			 umlPanel.redo();
-		 }
-		 else if (selectedCommand == selectAllText) {
-			 umlPanel.select();
-		 } else if (selectedCommand == optionsPaneObjectDraw ) {
+
+		 if (selectedCommand == optionsPaneObjectDraw ) {
 			 umlPanel.changeInteractionMode(new DrawMode(DrawableType.OBJECT, umlPanel));
 		 } else if (selectedCommand == optionsPaneRelationDraw ) {
 			 umlPanel.changeInteractionMode(new DrawMode(DrawableType.RELATION, umlPanel));
