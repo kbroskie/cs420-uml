bcba4d69bea835291c9617e6ee034bcf6c4d3a9c
Daniel Rabiega <rabiega.dan@gmail.com>
2014-04-01 12:09:24 -0400

    Lots of comments plus class fixes.

--- src/edu/millersville/cs/segfault/model/relation/Composition.java
+++ src/edu/millersville/cs/segfault/model/relation/Composition.java
@@ -11,25 +11,62 @@ import edu.millersville.cs.segfault.immutable.ImmutablePath;
 import edu.millersville.cs.segfault.immutable.ImmutablePoint;
 import edu.millersville.cs.segfault.model.DrawableType;
 
+/*****************************************************************************
+ * A subclass of UMLRelation which is ended by a filled diamond.
+ * 
+ * @author Daniel Rabiega
+ */
 public class Composition extends UMLRelation {
+	
+	//************************************************************************
+	// Static Members
 	
 	private static int[] xPoints = {0, 10, 0, -10, 0};
 	private static int[] yPoints = {0, -20, -30, -20, 0};
 
+	//************************************************************************
+	// Constructors
 	
-	
+	/*************************************************************************
+	 * Creates a new Composition with the given properties.
+	 */
 	public Composition(ImmutablePath path, int z, boolean selected) {
 		super(path, z, selected);
 	}
 	
+	/*************************************************************************
+	 * Recreates a Composition from an XML representation
+	 */
 	public Composition(String serialized) 
 		throws Exception
 	{
 		super(serialized);
 	}
 	
+	//************************************************************************
+	// Observers
+	
+	/*************************************************************************
+	 * Returns this composition's type, COMPOSITION
+	 */
 	public DrawableType getType() { return DrawableType.COMPOSITION; }
 	
+	//************************************************************************
+	// Mutators
+	
+	/*************************************************************************
+	 * Returns a copy of this composition with a new line segment.
+	 */
+	public Composition extend(ImmutablePoint p) {
+		return new Composition(this.getPath().addLast(p), this.getZ(), this.isSelected());
+	}
+	
+	//************************************************************************
+	// Drawing Methods
+	
+	/*************************************************************************
+	 * Draws an filled diamond at this aggregation's endpoint.
+	 */
 	public void drawArrow(Graphics g) {
 		if (this.getPath().size() >= 2) {
 			Iterator<ImmutableLine> lIter = this.getPath().lineIterator();
@@ -54,7 +91,5 @@ public class Composition extends UMLRelation {
 		}
 	}
 	
-	public Composition extend(ImmutablePoint p) {
-		return new Composition(this.getPath().addLast(p), this.getZ(), this.isSelected());
-	}
+	
 }
