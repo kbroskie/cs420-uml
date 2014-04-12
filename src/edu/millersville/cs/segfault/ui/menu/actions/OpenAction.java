c3e7eb6a91c3e686b67946e7404d4f1d023c28dc
creativebluebutterfly <kbroskie@gmail.com>
2014-04-12 02:39:55 -0400

    Create OpenAction.java

---
+++ src/edu/millersville/cs/segfault/ui/menu/actions/OpenAction.java
@@ -0,0 +1,55 @@
+package edu.millersville.cs.segfault.ui.menu.actions;
+
+import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import edu.millersville.cs.segfault.ui.UMLWindow;
+import edu.millersville.cs.segfault.ui.menu.MenuAction;
+import edu.millersville.cs.segfault.ui.menu.ActionType;
+
+/**************************************************************************
+ * SaveAction is the class responsible for invoking the method to 
+ * load a model.
+ * @author Kimberlyn Broskie
+ *************************************************************************/
+public class OpenAction extends AbstractAction
+						implements MenuAction {
+
+	//*************************************************************************
+	// Static Instance Variables
+	//*************************************************************************
+	private static final long serialVersionUID = -401022597982490050L;
+	private static final String openMenuText = "Open";
+
+	//*************************************************************************
+	// Instance Variables
+	//*************************************************************************
+	private final UMLWindow window;
+
+	/**************************************************************************
+	* Constructor that builds the action with an accelerator.
+	* @param win the frame for the interface.
+	*************************************************************************/
+	public OpenAction (UMLWindow win)
+	{
+		super(openMenuText);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, Event.CTRL_MASK));		
+		window = win;
+	}
+	
+	//*************************************************************************
+	// Observers
+	//*************************************************************************
+	public ActionType getType() { return ActionType.OPEN; }	
+
+	//*************************************************************************
+	// Event Listeners
+	//*************************************************************************
+	public void actionPerformed(ActionEvent se) {
+		window.getUMLPanel().load();
+	}		
+}
