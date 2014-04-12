79176b8244a257043949ea1efcc49eaa8c41c6be
creativebluebutterfly <kbroskie@gmail.com>
2014-04-12 02:39:36 -0400

    Create NewAction.java

---
+++ src/edu/millersville/cs/segfault/ui/menu/actions/NewAction.java
@@ -0,0 +1,56 @@
+package edu.millersville.cs.segfault.ui.menu.actions;
+
+import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import edu.millersville.cs.segfault.model.UMLModel;
+import edu.millersville.cs.segfault.ui.UMLWindow;
+import edu.millersville.cs.segfault.ui.menu.MenuAction;
+import edu.millersville.cs.segfault.ui.menu.ActionType;
+
+/**************************************************************************
+ * NewAction is the class responsible for invoking the method to 
+ * create a new model.
+ * @author Kimberlyn Broskie
+ *************************************************************************/
+public class NewAction extends AbstractAction 
+					implements MenuAction {
+	
+	//*************************************************************************
+	// Static Instance Variables
+	//*************************************************************************
+	private static final long serialVersionUID = 447204292723445794L;
+	private static final String newMenuText = "New";
+
+	//*************************************************************************
+	// Instance Variables
+	//*************************************************************************
+	private final UMLWindow window;
+	
+	/**************************************************************************
+	* Constructor that builds the action with an accelerator.
+	* @param win the frame for the interface.
+	*************************************************************************/
+	public NewAction (UMLWindow win) 
+	{
+		super(newMenuText);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, Event.CTRL_MASK));		
+		window = win;
+	}
+	
+	//*************************************************************************
+	// Observers
+	//*************************************************************************
+	public ActionType getType() { return ActionType.NEW; }	
+	
+	//*************************************************************************
+	// Event Listeners
+	//*************************************************************************
+	public void actionPerformed(ActionEvent se) {
+		window.getUMLPanel().changeModel(new UMLModel());
+	}	
+}
