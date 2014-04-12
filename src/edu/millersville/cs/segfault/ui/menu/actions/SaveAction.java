5c5713d7b53df35f36d7ec5638b38478ab654ff5
creativebluebutterfly <kbroskie@gmail.com>
2014-04-12 02:40:44 -0400

    Create SaveAction.java

---
+++ src/edu/millersville/cs/segfault/ui/menu/actions/SaveAction.java
@@ -0,0 +1,57 @@
+package edu.millersville.cs.segfault.ui.menu.actions;
+
+import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import edu.millersville.cs.segfault.ui.UMLWindow;
+import edu.millersville.cs.segfault.ui.menu.MenuAction;
+import edu.millersville.cs.segfault.ui.menu.ActionType;
+
+
+/**************************************************************************
+ * SaveAction is the class responsible for invoking the method to save the 
+ * current state of the model.
+ * @author Kimberlyn Broskie
+ *************************************************************************/
+public class SaveAction extends AbstractAction 
+						implements MenuAction{
+
+	//*************************************************************************
+	// Static Instance Variables
+	//*************************************************************************
+	private static final long serialVersionUID = -7422025816728887633L;
+	private static final String saveMenuText = "Save";
+	
+	//*************************************************************************
+	// Instance Variables
+	//*************************************************************************
+	private final UMLWindow window;
+
+	/**************************************************************************
+	* Constructor that builds the action with an accelerator.
+	* @param win the frame for the interface.
+	*************************************************************************/
+	public SaveAction (UMLWindow win)
+	{
+		super(saveMenuText);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, Event.CTRL_MASK));		
+		window = win;
+	}
+	
+	//*************************************************************************
+	// Observers
+	//*************************************************************************
+	public ActionType getType() { return ActionType.SAVE; }	
+	
+	//*************************************************************************
+	// Event Listeners
+	//*************************************************************************
+	public void actionPerformed(ActionEvent se) {
+		window.getUMLPanel().save(
+				window.getUMLPanel().getModel().serialize());
+	}	
+}
